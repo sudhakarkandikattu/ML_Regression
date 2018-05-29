9d1a111660cc379e12ba4d80fb47209a66573470
sudhakarkandikattu <32635960+sudhakarkandikattu@users.noreply.github.com>
2018-05-29 20:31:24 +0530

    Update Linear_regression.py

--- Linear_regression.py
+++ Linear_regression.py
@@ -1,5 +1,4 @@
 #simple leniar regression
-# Data Preprocessing Template
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -47,11 +46,3 @@ plt.show()
 
 
 
-
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
